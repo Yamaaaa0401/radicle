09b32f5b32b75a041a9934d5aa3c7fb0a748073b
YamaguchiKatsuya <rt08863ky@gmail.com>
2020-09-07 23:30:17 +0900

    復旧

---
+++ database/factories/SeminarFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Seminar;
+use Faker\Generator as Faker;
+
+$factory->define(Seminar::class, function (Faker $faker) {
+    return [
+        //テストデータを入れる
+
+
+    ];
+});
