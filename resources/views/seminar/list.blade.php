5b4534f320d6e5e1c013dc9961d5a54f72a9ffa7
YamaguchiKatsuya <rt08863ky@gmail.com>
2020-09-08 00:24:42 +0900

    レイアウトの使いまわしコード作成

--- resources/views/seminar/list.blade.php
+++ resources/views/seminar/list.blade.php
@@ -1,53 +1,23 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>セミナー</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="/js/app.js" defer></script>
-</head>
-<body>
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">セミナー</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">セミナー一覧 <span class="sr-only"></span></a>
-      <a class="nav-item nav-link" href="#">セミナー投稿</a>
-    </div>
-  </div>
-</nav>
-    </header>
-    <br>
-    <div class="container">
-    <div class="row">
+@extends('layout')
+@section('title','セミナー一覧')
+@section('content')
+<div class="row">
   <div class="col-md-8 col-md-offset-2">
-      <h2>セミナー一覧</h2>
-      <table class="table table-striped">
-          <tr>
-              <th>セミナー番号</th>
-              <th>開催日付</th>
-              <th>タイトル</th>
-              <th></th>
-          </tr>
-          <tr>
-              <td>1</td>
-              <td>2020/06/30</td>
-              <td>テスト</td>
-              <td></td>
-          </tr>
-      </table>
+    <h2>セミナー一覧</h2>
+    <table class="table table-striped">
+      <tr>
+        <th>セミナー番号</th>
+        <th>開催日付</th>
+        <th>タイトル</th>
+        <th></th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2020/06/30</td>
+        <td>テスト</td>
+        <td></td>
+      </tr>
+    </table>
   </div>
 </div>
-    </div>
-    <footer class="footer bg-dark  fixed-bottom">
-    <div class="container text-center">
-    <span class="text-light">©︎やまのラジ来るプログラミング練習</span>
-</div>
-    </footer>
-</body>
-</html>
+@endsection
