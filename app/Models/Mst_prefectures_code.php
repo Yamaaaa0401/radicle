59e980f67bb34335ac0ed691e21892fc3d857066
YamaguchiKatsuya <rt08863ky@gmail.com>
2020-09-20 22:15:03 +0900

    都道府県MODEL作成

---
+++ app/Models/Mst_prefectures_code.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mst_prefectures_code extends Model
+{
+    //
+    //テーブル名
+    protected $table = 'seminars';
+
+    //可変項目
+    protected $fillable = 
+    [
+        //都道府県コード変更はなし
+    ];
+
+    
+}
